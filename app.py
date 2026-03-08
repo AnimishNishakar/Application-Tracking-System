37e4f735a28ef2c5d98d57dda841fcb32d15ead7
Animish Nishakar <105592560+AnimishNishakar@users.noreply.github.com>
2026-03-09 02:01:04 +0530

    Change model name to 'gemini-3-flash-preview'
    Updated model name and formatted response generation calls for clarity.

--- app.py
+++ app.py
@@ -6,16 +6,22 @@ import base64
 from google import genai
 
 client = genai.Client(api_key=st.secrets["GOOGLE_API_KEY"])
-MODEL_NAME = "gemini-2.5-flash"
+MODEL_NAME = "gemini-3-flash-preview"
 # Define cached functions
 @st.cache_data()
 def get_gemini_response(input, pdf_content, prompt):
-    response = client.models.generate_content(model=MODEL_NAME, contents=[input, pdf_content[0], prompt])
+    response = client.models.generate_content(
+        model=MODEL_NAME,
+        contents=[input, pdf_content[0], prompt]
+    )
     return response.text
 
 @st.cache_data()
 def get_gemini_response_keywords(input, pdf_content, prompt):
-    response = client.models.generate_content(model=MODEL_NAME, contents=[input, pdf_content[0], prompt])
+    response = client.models.generate_content(
+        model=MODEL_NAME,
+        contents=[input, pdf_content[0], prompt]
+    )
     return json.loads(response.text[8:-4])
 
 @st.cache_data()
